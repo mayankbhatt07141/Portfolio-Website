d820bc63e772ea50633ac3380e62629a8bea4a3c
Mayank <mayankbhatt07141@gmail.com>
2021-08-29 00:35:29 +0530

    feat-responsive  header

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import linksbutton from "../Assets/hamburger.png";
 function Navbar() {
+  const [showlinks, setShowlinks] = useState(false);
   return (
     <div className="navbar">
+      //{" "}
       <div className="left-nav">
-        <div className="links-container">
+        <div className="links-container" id={showlinks ? "open-links" : ""}>
           <Link to="/" className="links">
             Home
           </Link>
-
           <Link to="/skills" className="links">
             Skills
           </Link>
-
           <Link to="/about" className="links">
             About
           </Link>
@@ -21,6 +22,14 @@ function Navbar() {
             Feedback
           </Link>
         </div>
+        <button
+          className="links-button"
+          onClick={() => {
+            setShowlinks(!showlinks);
+          }}
+        >
+          <img src={linksbutton} />
+        </button>
       </div>
       <div className="right-nav">
         <h1 className="logo">logo</h1>
